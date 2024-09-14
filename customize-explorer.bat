1c7f3fe68bc0b9418768568d1c3470386a160102
dorvillethiago <dorville.dev@gmail.com>
2024-09-14 15:54:32 -0300

    Customize Windows Explorer appearance

--- customize-explorer.bat
+++ customize-explorer.bat
@@ -1,11 +1,13 @@
 @echo off
 
-:: Disable minimize/maximize animation
-powershell -Command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'MinAnimate' -Value 0"
+:: Set system mode (Windows interface) to Dark
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
 
-:: Restart Explorer to apply the changes
-taskkill /f /im explorer.exe
-start explorer.exe
+:: Set app mode (applications and settings) to Dark
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+
+:: Enable taskbar auto-hide using PowerShell
+powershell -Command "&{$key='HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3'; $bytes=(Get-ItemProperty -Path $key).Settings; $bytes[8]=03; Set-ItemProperty -Path $key -Name Settings -Value $bytes; Stop-Process -f -ProcessName explorer}"
 
 echo All customizations have been aplied.
 pause
