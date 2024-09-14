545965ab5dc9945862b727256dd0b8e1843ecc29
dorvillethiago <dorville.dev@gmail.com>
2024-09-14 10:53:20 -0300

    first commit

---
+++ install-apps.bat
@@ -0,0 +1,43 @@
+@echo off
+
+echo Installing Arc Browser
+winget install --id TheBrowserCompany.Arc -e
+
+echo Installing 7zip
+winget install --id 7zip.7zip -e
+
+echo Installing Git...
+winget install --id Git.Git -e
+
+echo Installing GitHub CLI...
+winget install --id GitHub.cli -e
+
+echo Installing Parsec...
+winget install --id Parsec.Parsec -e
+
+echo Installing NVM for Windows...
+winget install --id CoreyButler.NVMforWindows -e
+
+echo Installing Visual Studio Code...
+winget install --id Microsoft.VisualStudioCode -e
+
+echo Installing Termius...
+winget install --id Termius.Termius -e
+
+echo Installing WhatsApp...
+winget install --id 9NKSQGP7F2NH -e
+
+echo Installing Figma...
+winget install --id Figma.Figma -e
+
+echo Installing Docker...
+winget install --id Docker.DockerDesktop -e
+
+echo Installing Discord...
+winget install --id Discord.Discord -e
+
+echo Installing Python 3.10...
+winget install --id Python.Python.3.10 -e
+
+echo Instalation complete!
+pause
