215b2f5c86eecdb98911f1b3380c12d3664c3601
dorvillethiago <dorville.dev@gmail.com>
2025-02-24 14:46:08 -0300

    feat: enhance install-apps.bat with improved echo statements for better installation feedback

--- install-apps.bat
+++ install-apps.bat
@@ -1,58 +1,76 @@
 @echo off
 
-echo Installing Arc Browser
+echo Installing Arc Browser...
 winget install --id TheBrowserCompany.Arc -e
+echo.
 
-echo Installing Zen Browser
-winget install --id Zen-Team.Zen-Browser  -e
+echo Installing Zen Browser...
+winget install --id Zen-Team.Zen-Browser -e
+echo.
 
-echo Installing 7zip
+echo Installing 7zip...
 winget install --id 7zip.7zip -e
+echo.
 
 echo Installing Git...
 winget install --id Git.Git -e
+echo.
 
 echo Installing GitHub CLI...
 winget install --id GitHub.cli -e
+echo.
 
 echo Installing Parsec...
 winget install --id Parsec.Parsec -e
+echo.
 
 echo Installing NVM for Windows...
 winget install --id CoreyButler.NVMforWindows -e
+echo.
 
 echo Installing Visual Studio Code...
 winget install --id Microsoft.VisualStudioCode -e
+echo.
 
 echo Installing Termius...
 winget install --id Termius.Termius -e
+echo.
 
 echo Installing WhatsApp...
-winget install --id 9NKSQGP7F2NH -e
+winget install --id WhatsApp.WhatsApp -e
+echo.
 
 echo Installing Figma...
 winget install --id Figma.Figma -e
+echo.
 
 echo Installing Docker...
 winget install --id Docker.DockerDesktop -e
+echo.
 
 echo Installing Discord...
 winget install --id Discord.Discord -e
+echo.
 
 echo Installing Notion...
 winget install --id Notion.Notion -e
+echo.
 
 echo Installing NordPass...
 winget install --id NordPassTeam.NordPass -e
+echo.
 
 echo Installing Python 3.10...
 winget install --id Python.Python.3.10 -e
+echo.
 
-echo Installing OhMyPosh
+echo Installing OhMyPosh...
 winget install --id JanDeDobbeleer.OhMyPosh -e
+echo.
 
-echo Installing GlazeWM
-winget install --id glzr-io.glazewm  -e
+echo Installing GlazeWM...
+winget install --id glzr-io.glazewm -e
+echo.
 
-echo Instalation complete!
+echo Installation complete!
 pause
